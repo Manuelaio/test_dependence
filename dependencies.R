5dfc10b69c50d739a72cbda3bb6b1e9e8fd31f70
Manuelaio <41114498+Manuelaio@users.noreply.github.com>
2020-01-14 14:39:42 +0100

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -1,4 +1,3 @@
-
 setRepositories(ind = 1:5)
 install.packages(c('shiny', 'shinydashboard'), repos='http://cran.rstudio.com/')
 install.packages('BiocManager')
@@ -7,24 +6,18 @@ list_of_packages <- c("shinyWidgets", "BiocInstaller","shinyWidgets","shinyBS","
                       "OrganismDbi", "stringr", "shinyjs", "condformat","ggbio", "bedr", "org.Hs.eg.db", "rtracklayer","stats",
                       "Rsamtools", "TxDb.Hsapiens.UCSC.hg19.knownGene","TxDb.Hsapiens.UCSC.hg38.knownGene",
                       "BSgenome.Hsapiens.UCSC.hg19", "BSgenome.Hsapiens.UCSC.hg38",
-                      "EnsDb.Hsapiens.v75","EnsDb.Hsapiens.v86")
-new.packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-my_packages= installed.packages()[,1]
-check_require <- function(package){
-  if(eval(parse(text=paste("require(",package,")")))) return(paste0(package, ' works in environment rstudio \n'))
-  else(return(paste0(package,' error! No package calls \n')))
-  suppressPackageStartupMessages(require(package))
-  
+                      "EnsDb.Hsapiens.v75","EnsDb.Hsapiens.v86", "ranger", "caret")
+
+lst_to_install <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]
+
+n_cores <- parallel::detectCores()
+install.packages(lst_to_install, Ncpus = n_cores - 1, clean = TRUE)
+
+not_installed <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]
+
+if(length(not_installed) == 0) {
+  print("Good to go!")
+} else {
+  print("You need to install these packages!")
+  not_installed
 }
-
-a=list()
-
-for (i in my_packages){
-  
-  date=(check_require(i))
-  # a= list.append(a, i, date)
-  a=append(a,date)
-  write.table(a, file='./library.txt', quote=FALSE, row.names = FALSE, col.names = FALSE)
-}
-
