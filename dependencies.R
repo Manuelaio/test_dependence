3a4904fe4fb32ef1d21875836d2922384dd6b1a3
Manuelaio <41114498+Manuelaio@users.noreply.github.com>
2020-01-14 13:29:26 +0100

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -10,4 +10,21 @@ list_of_packages <- c("shinyWidgets", "BiocInstaller","shinyWidgets","shinyBS","
                       "EnsDb.Hsapiens.v75","EnsDb.Hsapiens.v86")
 new.packages <- list_of_packages[!(list_of_packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
+my_packages= installed.packages()[,1]
+check_require <- function(package){
+  if(eval(parse(text=paste("require(",package,")")))) return(paste0(package, ' works in environment rstudio \n'))
+  else(return(paste0(package,' error! No package calls \n')))
+  suppressPackageStartupMessages(require(package))
+  
+}
+
+a=list()
+
+for (i in my_packages){
+  
+  date=(check_require(i))
+  # a= list.append(a, i, date)
+  a=append(a,date)
+  write.table(a, file='./library.txt', quote=FALSE, row.names = FALSE, col.names = FALSE)
+}
 
