c0acc38c49b35a18a9bf6604a8dba45e30eeb483
Manuelaio <41114498+Manuelaio@users.noreply.github.com>
2020-01-22 18:06:09 +0100

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -9,14 +9,24 @@ list_of_packages <- c("shinyWidgets", "BiocInstaller","DT","shinyWidgets","shiny
                       "EnsDb.Hsapiens.v75","EnsDb.Hsapiens.v86")
 
 lst_to_install <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]
-BiocInstaller::biocLite('Gviz')
-BiocInstaller::biocLite('ggbio')
+
 n_cores <- parallel::detectCores()
 install.packages(lst_to_install, Ncpus = n_cores - 1, clean = TRUE)
 
 not_installed <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]
 
-if(length(not_installed) == 0) {
+cheeck= function(not_installed){
+if(isEmpty(not_installed))
+print('GO! All packages are installed')
+else if (not_installed !=0 & not_installed %in% 'Gviz')
+return(BiocInstaller::biocLite('Gviz'))
+else if(not_installed %in% 'ggbio')
+return(BiocInstaller::biocLite('ggbio'))}
+cheeck(not_installed)  
+
+not_installed_yet <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]   
+
+if(length(not_installed_yet) == 0) {
   print("Good to go!")
 } else {
   print("You need to install these packages!")
