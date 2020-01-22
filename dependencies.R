ffaf57c8c1a2b0b0a24a5c82a419a7335bf367a5
Manuelaio <41114498+Manuelaio@users.noreply.github.com>
2020-01-22 19:37:33 +0100

    Update dependencies.R

--- dependencies.R
+++ dependencies.R
@@ -2,33 +2,23 @@ setRepositories(ind = 1:5)
 install.packages(c('shiny', 'shinydashboard'), repos='http://cran.rstudio.com/')
 install.packages('BiocManager')
 BiocManager::install('BiocInstaller')
-list_of_packages <- c("shinyWidgets", "BiocInstaller","DT","shinyWidgets","shinyBS","markdown", "data.table", "DT","dplyr", "Gviz", "Homo.sapiens",
+list_of_packages <- c("shinyWidgets", "BiocInstaller","shinyWidgets","shinyBS","markdown","DT", "data.table", "dplyr", "Gviz", "Homo.sapiens",
                       "OrganismDbi", "stringr", "shinyjs", "condformat","ggbio", "bedr", "org.Hs.eg.db", "rtracklayer","stats",
                       "Rsamtools", "TxDb.Hsapiens.UCSC.hg19.knownGene","TxDb.Hsapiens.UCSC.hg38.knownGene",
                       "BSgenome.Hsapiens.UCSC.hg19", "BSgenome.Hsapiens.UCSC.hg38",
-                      "EnsDb.Hsapiens.v75","EnsDb.Hsapiens.v86")
+                      "EnsDb.Hsapiens.v75","EnsDb.Hsapiens.v86", "ranger", "caret")
 
 lst_to_install <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]
 
 n_cores <- parallel::detectCores()
 install.packages(lst_to_install, Ncpus = n_cores - 1, clean = TRUE)
-
+BiocInstaller::biocLite('Gviz')
+BiocInstaller::biocLite('ggbio')
 not_installed <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]
 
-cheeck= function(not_installed){
-if(isEmpty(not_installed))
-print('GO! All packages are installed')
-else if (not_installed !=0 & not_installed %in% 'Gviz')
-return(BiocInstaller::biocLite('Gviz'))
-else if(not_installed %in% 'ggbio')
-return(BiocInstaller::biocLite('ggbio'))}
-cheeck(not_installed)  
-
-not_installed_yet <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]   
-
-if(length(not_installed_yet) == 0) {
+if(length(not_installed) == 0) {
   print("Good to go!")
 } else {
   print("You need to install these packages!")
-  not_installed_yet
+  not_installed
 }
