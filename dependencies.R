d29217a761525b7c3fe90af3ab756db781bd2d4b
Manuelaio <41114498+Manuelaio@users.noreply.github.com>
2020-05-12 15:28:19 +0200

    update dep

--- dependencies.R
+++ dependencies.R
@@ -2,11 +2,12 @@ setRepositories(ind = 1:5)
 install.packages(c('shiny', 'shinydashboard'), repos='http://cran.rstudio.com/')
 install.packages('BiocManager')
 #BiocManager::install('BiocInstaller')
-list_of_packages <- c("shinyWidgets", "BiocInstaller","shinyWidgets","shinyBS","markdown","DT", "data.table", "dplyr", "Gviz", "Homo.sapiens",
-                      "OrganismDbi", "stringr", "shinyjs", "condformat","ggbio", "bedr", "org.Hs.eg.db", "rtracklayer","stats",
-                      "Rsamtools", "TxDb.Hsapiens.UCSC.hg19.knownGene","TxDb.Hsapiens.UCSC.hg38.knownGene",
-                      "BSgenome.Hsapiens.UCSC.hg19", "BSgenome.Hsapiens.UCSC.hg38",
-                      "EnsDb.Hsapiens.v75","EnsDb.Hsapiens.v86", "ranger", "caret")
+list_of_packages <- c("markdown", "shiny", "shinyjs", "shinyBS", "shinyWidgets","shinycssloaders", "DT", 
+                      "Gviz","dplyr", "Homo.sapiens", "openxlsx", "condformat", "stringr", "bedr", 
+                      "org.Hs.eg.db", "TxDb.Hsapiens.UCSC.hg38.knownGene", 
+                      "TxDb.Hsapiens.UCSC.hg19.knownGene", "rlist",
+                      "EnsDb.Hsapiens.v75", "EnsDb.Hsapiens.v86", "OrganismDbi",
+                      "BSgenome.Hsapiens.UCSC.hg19", "processx", "Rsamtools", "GenomicRanges", "BSgenome.Hsapiens.UCSC.hg38")
 
 lst_to_install <- list_of_packages[!(list_of_packages %in% installed.packages()[, "Package"])]
 
